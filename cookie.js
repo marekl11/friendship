3b2cfa08d954f7d8502d8ceb2e02b4ac71223d2a
Lukáš Marek <mareksakul@gmail.com>
2024-06-13 00:03:27 +0200

    dynamic bar color

--- cookie.js
+++ cookie.js
@@ -30,13 +30,30 @@ function getProgress() {
 function checkAccess(orderNumber) {
     var progress = getProgress();
     var messageElement = document.getElementById("message");
+    var themeMetaTag = document.querySelector('meta[name="theme-color"]');
     if (progress < orderNumber) {
-        messageElement.innerHTML = "We are\n not so far\n into our\n friendship";
+        messageElement.innerHTML = "We are<br> not so far<br> into our<br> friendship";
         messageElement.style.color = "#333"; // Black text for locked state
+        if (themeMetaTag) {
+            themeMetaTag.setAttribute("content", "#ffffff"); // White theme color for locked state
+        } else {
+            var newMetaTag = document.createElement('meta');
+            newMetaTag.name = "theme-color";
+            newMetaTag.content = "#ffffff";
+            document.head.appendChild(newMetaTag);
+        }
     } else {
         document.body.classList.add("unlocked");
         messageElement.style.color = "white"; // White text for unlocked state
         document.getElementById("content").style.display = "block";
+        if (themeMetaTag) {
+            themeMetaTag.setAttribute("content", "#000000"); // Black theme color for unlocked state
+        } else {
+            var newMetaTag = document.createElement('meta');
+            newMetaTag.name = "theme-color";
+            newMetaTag.content = "#000000";
+            document.head.appendChild(newMetaTag);
+        }
         if (progress < orderNumber + 1) {
             setCookie("progress", orderNumber + 1, 365);
         }
